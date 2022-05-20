e88d73c26306d062754fac0ae7b9fc35d8ac7ab7
Rahul Rajeevan <rahul.rajeevan4@gmail.com>
2022-05-20 20:05:18 +0530

    added

---
+++ src/components/Abc.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Abc = () => {
+  return (
+    <div>
+        <h2>Mobile Operating System</h2>
+        <ul>
+          <li>Android</li>
+          <li>Blackberry</li>
+          <li>iPhone</li>
+          <li>Windows Phone</li>
+        </ul>
+        <h2>Mobile Manufacturers</h2>
+        <ul>
+          <li>Samsung</li>
+          <li>HTC</li>
+          <li>Micromax</li>
+          <li>Apple</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Abc
